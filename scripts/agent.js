d11338d99229c6c130d41c9e75bcae2f06b2442f
Jose Saldaña <jose.saldana@nazaries.com>
2021-06-05 19:41:28 +0200

    Finished basic functionality

--- scripts/agent.js
+++ scripts/agent.js
@@ -1,8 +1,17 @@
 class Agent {
     sockets;
+    acStatus;
+    luxStatus;
 
     constructor(io) {
         this.sockets = io.sockets;
+        this.acStatus = false;
+        this.luxStatus = false;
+        this.initalizeCommunications();
+        console.log("Agente ready....");
+    }
+    
+    initalizeCommunications() {
         this.sockets.on('connection', (controller) => {
             controller.on('updateTmp', (updateTmp) => {
                 this.manage(updateTmp);
@@ -10,28 +19,43 @@ class Agent {
             controller.on('updateLux', (updateLux) => {
                 this.manage(updateLux);
             });
+            controller.on('clientAction', (changes) => {
+                console.log("Agente escucha al cliente cambiar el estado de", changes.toggled);
+                let action;
+                if (changes.toggled === 'ac') {
+                    this.acStatus = !this.acStatus;
+                    action = this.acStatus ? "acOn" : "acOff";
+                } else if (changes.toggled === 'lux') {
+                    this.luxStatus = !this.luxStatus;
+                    action = this.luxStatus ? "openWindow" : "closeWindow";
+                }
+                this.sockets.emit(action);
+            });
         });
-        console.log("Agente ready....");
     }
-    
+
     manage(newData) {
         console.log("AGENTE escucha:", newData);
         switch (newData.type) {
             case 'tmp':
-                if (newData.value > 32) {
+                if (newData.value > 32 && !this.acStatus) {
                     console.log("AGENTE enciende aire acondicionado");
+                    this.acStatus = true;
                     this.sockets.emit("acOn");
-                } else if (newData.value < 24) {
+                } else if (newData.value < 24 && this.acStatus) {
                     console.log("AGENTE apaga aire acondicionado");
+                    this.acStatus = false;
                     this.sockets.emit("acOff");
                 }
                 break;
             case 'lux':
-                if (newData.value > 300) {
+                if (newData.value > 300 && this.luxStatus) {
                     console.log("AGENTE cierra la persiana");
+                    this.luxStatus = false;
                     this.sockets.emit("closeWindow");
-                } else if (newData.value < 200) {
+                } else if (newData.value < 200 && !this.luxStatus) {
                     console.log("AGENTE abre la persiana");
+                    this.luxStatus = true;
                     this.sockets.emit("openWindow");
                 }
                 break;
